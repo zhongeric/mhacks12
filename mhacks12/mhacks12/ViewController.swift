faedec5ed927385980edb62d6bc69872e3323adb
Eric Zhong <ezhong1900@gmail.com>
2019-10-11 22:38:16 -0400

    new commit

---
+++ mhacks12/mhacks12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mhacks12
+//
+//  Created by Eric Zhong on 10/11/19.
+//  Copyright © 2019 ericzhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mhacks12/mhacks12Tests/mhacks12Tests.swift
@@ -0,0 +1,34 @@
+//
+//  mhacks12Tests.swift
+//  mhacks12Tests
+//
+//  Created by Eric Zhong on 10/11/19.
+//  Copyright © 2019 ericzhong. All rights reserved.
+//
+
+import XCTest
+@testable import mhacks12
+
+class mhacks12Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
